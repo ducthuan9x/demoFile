1d3ec2552fcf6cc60505409704ed803798ebb57b
thuan <ducthuan051291@gmail.com>
2022-09-14 10:06:49 +0700

    docfile

---
+++ src/com/daicent/main/Test.java
@@ -0,0 +1,19 @@
+package com.daicent.main;
+
+import java.io.File;
+
+public class Test {
+    public static long fileGetSize(String name){
+        File file=new File(name);
+        if (!file.exists()|| !file.isFile()){
+            System.out.println("file ko tồn tại");
+            return -1;
+        }
+        return file.length();
+    }
+
+    public static void main(String[] args) {
+        long size= fileGetSize("D:\\test_Flinters\\demoFile\\src\\demo.txt");
+        System.out.println("so ky tu trong file = " + size);
+    }
+}
